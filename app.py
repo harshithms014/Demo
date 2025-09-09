d457bee4553f8cf7e9f4173f097cd4bb3b418f4e
harshithms014 <harshithms014>
2025-09-09 17:18:10 +0530

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Multi-Stage Docker Build!Created by harshithms014 ths to check and deploy using kubernetes   "
+    return "Hello from Multi-Stage Docker Build!Created by harshithms014   "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5006)
