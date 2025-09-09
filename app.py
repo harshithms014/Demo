008cd6dccdcf4d59c246aee67e45de67361a4f54
harshithms014 <harshithms014>
2025-09-09 17:08:24 +0530

    adding varification branch

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Multi-Stage Docker Build!Created by harshithms014"
+    return "Hello from Multi-Stage Docker Build!Created by harshithms014 ths to check "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5006)
